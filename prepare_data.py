8e1547996694bea723a5aae36396bda073e801e4
Nguyễn Quang Tính <100455964+tinhpx2911@users.noreply.github.com>
2023-06-15 14:25:45 +0700

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -11,7 +11,7 @@ import numpy as np
 from datasets import load_dataset
 
 # download the Vietnamese poetry dataset
-dataset_name = 'truongpdd/vietnamese_poetry'
+dataset_name = 'bigscience-data/roots_vi_vietnamese_poetry'
 dataset = load_dataset(dataset_name)
 data = "\n".join(dataset['train'][:]['text'])
 
