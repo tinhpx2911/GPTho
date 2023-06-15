90eeb3902ddbd46a4077fdc01d1e84f1e44feeb2
Nguyễn Quang Tính <100455964+tinhpx2911@users.noreply.github.com>
2023-06-15 11:49:35 +0700

    Update train_gpt.py

--- train_gpt.py
+++ train_gpt.py
@@ -30,9 +30,9 @@ from model_gpt import GPTConfig, GPT
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out'
-eval_interval = 1000
+eval_interval = 500
 log_interval = 1
-eval_iters = 100
+eval_iters = 50
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
