0db322476305207095700bafb51ab49ab651bdd6
Nguyễn Quang Tính <100455964+tinhpx2911@users.noreply.github.com>
2023-06-21 12:03:18 +0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@
 Sample from a trained model
 """
 import os
+import random
 import pickle
 from contextlib import nullcontext
 import torch
@@ -11,11 +12,11 @@ from model_gpt import GPTConfig, GPT
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out' # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10 # number of samples to draw
+num_samples = 5 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-seed = 1337
+seed = random.randint(1,1000)
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
